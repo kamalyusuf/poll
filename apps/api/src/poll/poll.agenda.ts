0b9352af644440e092b886d404a27513184a4d34
Kamal <kbomoi@yahoo.com>
2023-05-28 00:43:09 +0300

    dont delete votes after poll ended

--- apps/api/src/poll/poll.agenda.ts
+++ apps/api/src/poll/poll.agenda.ts
@@ -1,7 +1,6 @@
 import { Types } from "mongoose";
 import { agenda } from "../lib/agenda";
 import { Job } from "agenda";
-import { usetransaction } from "../utils";
 import { Poll } from "./poll.model";
 import { PollVote } from "./poll-vote.model";
 import { io } from "../lib/io";
@@ -15,12 +14,8 @@ agenda.define(
     const poll = await Poll.findById(job.attrs.data.poll_id);
 
     if (!poll) return Promise.resolve();
-
-    await usetransaction(async (session) => {
-      await poll.set({ status: "ended" }).save({ session });
-
-      return PollVote.deleteMany({ poll_id: poll._id }, { session });
-    });
+    
+    await poll.set({ status: "ended" });
 
     io.connection.to(poll._id.toString()).emit("poll ended", poll);
   }
